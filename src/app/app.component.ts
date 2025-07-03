a73bf784a2e7337e81bedb935cbd846a72a7ca21
Elisha <elisha03@graduate.utm.my>
2025-07-03 11:45:55 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,35 @@
 import { Component } from '@angular/core';
+<<<<<<< Updated upstream
 import { RouterModule } from '@angular/router'; // ✅ Needed for routerLink, routerLinkActive, routerLinkActiveOptions
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 
+=======
+import { Router, NavigationEnd, RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // ✅ Add this
+import { filter } from 'rxjs/operators';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-root',
   standalone: true,
+<<<<<<< Updated upstream
   imports: [RouterModule, RouterOutlet,FooterComponent], // ✅ Include both
+=======
+  imports: [CommonModule, RouterModule, RouterOutlet], // ✅ Include CommonModule here
+>>>>>>> Stashed changes
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'] // ✅ typo fix: should be styleUrls, not styleUrl
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'wildlife';
+  isHomepage = false;
 
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.isHomepage = event.urlAfterRedirects === '/';
+      });
+  }
 }
